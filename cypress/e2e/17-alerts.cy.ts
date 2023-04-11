105dd4fbb0c2b835698ffde98d6b6766796a8714
Sidnei Farias <sidneijr.77@gmail.com>
2023-04-11 20:12:48 -0300

    Inclui um caso de uso, com alerts do JavaScript

---
+++ cypress/e2e/17-alerts.cy.ts
@@ -0,0 +1,19 @@
+// https://docs.cypress.io/api/cypress-api/catalog-of-events#App-Events
+describe('Alerts example', () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env("theInternet")}/javascript_alerts`);
+  });
+
+  it('JS Alert', () => {
+    cy.contains('button', 'Click for JS Alert').click();
+
+    cy.on('window:alert', (message) => {
+      expect(message).to.be.equal("I am a JS Alert");
+    });
+
+    // Com "window:confirm", precisamos definir uma função que retorna um boolean, correspondente a se eu quero confirmar a mensagem ou não.
+    cy.on('window:confirm', () =>  true);
+
+    cy.get('p#result').should('have.text', "You successfully clicked an alert");
+  });
+});
